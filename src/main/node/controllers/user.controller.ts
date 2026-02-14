08e624a49d3d3469b65cbd4b1c1c63127ba26dad
clonar714-jpg <clonar714@gmail.com>
2026-02-13 18:20:31 -0700

    Update user.controller.ts

--- src/main/node/controllers/user.controller.ts
+++ src/main/node/controllers/user.controller.ts
@@ -1,5 +1,6 @@
-/** User controller (stub). */
+
 import type { Request, Response } from 'express';
 export function getUser(_req: Request, res: Response) {
   res.status(501).json({ error: 'Not implemented' });
 }
+
