b74c6d74b8900e879e26d79954cd9433b2d6da9f
clonar714-jpg <clonar714@gmail.com>
2026-02-13 18:21:15 -0700

    Update validation.middleware.ts

--- src/main/node/middlewares/validation.middleware.ts
+++ src/main/node/middlewares/validation.middleware.ts
@@ -1,5 +1,6 @@
-/** Validation middleware (stub). */
+
 import type { Request, Response, NextFunction } from 'express';
 export function validationMiddleware(_req: Request, _res: Response, next: NextFunction) {
   next();
 }
+
